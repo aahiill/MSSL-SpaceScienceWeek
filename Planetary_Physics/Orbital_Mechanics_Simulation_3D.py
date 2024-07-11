242536465157860ccaf7e9945ef62c07bd868706
aahil <aahilnamajee@gmail.com>
2024-07-11 15:34:20 +0100

    Added 2D simulation

--- Planetary_Physics/Orbital_Mechanics_Simulation_3D.py
+++ Planetary_Physics/Orbital_Mechanics_Simulation_3D.py
@@ -145,14 +145,14 @@ def meteoroid(Ejection_speed, ejection_angle):
 
 # Planetary data: orbital radius in AU and mass in kg
 planets = {
-    #'Mercury': {'orbital_radius': 0.39, 'mass': 0.33e24},
-    #'Venus': {'orbital_radius': 0.723, 'mass': 4.867e24},
+    'Mercury': {'orbital_radius': 0.39, 'mass': 0.33e24},
+    'Venus': {'orbital_radius': 0.723, 'mass': 4.867e24},
     'Earth': {'orbital_radius': 1, 'mass': 5.97e24},
     'Mars': {'orbital_radius': 1.524, 'mass': 0.64171e24},
     'Jupiter': {'orbital_radius': 5.2, 'mass': 1898.19e24},
-    #'Saturn': {'orbital_radius': 9.58, 'mass': 568.34e24},
-    #'Uranus': {'orbital_radius': 19.22, 'mass': 86.813e24},
-    #'Neptune': {'orbital_radius': 30.05, 'mass': 102.413e24},
+    'Saturn': {'orbital_radius': 9.58, 'mass': 568.34e24},
+    'Uranus': {'orbital_radius': 19.22, 'mass': 86.813e24},
+    'Neptune': {'orbital_radius': 30.05, 'mass': 102.413e24},
 }
 
 # Calculate orbits for each planet
@@ -164,7 +164,7 @@ for planet, data in planets.items():
     ax.plot(x, y, z, label=planet)
 
 # Calculate and plot the path of the meteoroid
-met_x, met_y, met_z = meteoroid(6, 240)  # Example values: speed in km/s, angle in degrees
+met_x, met_y, met_z = meteoroid(1, 270)  # Example values: speed in km/s, angle in degrees
 ax.plot(met_x, met_y, met_z, label='Meteoroid', linestyle='dashed')
 
 ax.scatter(0, 0, 0, label='Sun', color='yellow', s=100)
